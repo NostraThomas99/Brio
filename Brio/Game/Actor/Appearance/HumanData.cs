23d09d0ac138a632d0ee4ff909b244f06bb6537d
Minmoose <12839703+Minmoose@users.noreply.github.com>
2026-05-03 14:23:53 -0500

    fix hair and skin color except for Hrothgar

--- Brio/Game/Actor/Appearance/HumanData.cs
+++ Brio/Game/Actor/Appearance/HumanData.cs
@@ -7,6 +7,7 @@ public class HumanData
     private readonly uint[] _rawColors;
 
     const int HairLength = 208;
+    const int SkinLength = 192;
     const uint NeutralHair = 0xFFFFFFFF;
 
     public HumanData(byte[] buffer)
@@ -22,21 +23,19 @@ public class HumanData
     public uint[] GetSkinColors(Tribes tribe, Genders gender)
     {
         var start = GetTribeSkinStartIndex(tribe, gender);
-        return _rawColors[start..(start + 192)];
+        return _rawColors[start..(start + SkinLength)];
     }
 
-    //TODO fix (ken) this is wrong but I can't math right now, too sick
-    public uint[] GetHairColors(Tribes tribe, Genders gender) => [.. _rawColors[256..(256 + HairLength)].Where(x => x != NeutralHair)];
-    //{
-    //    var start = GetTribeHairStartIndex(tribe, gender);
-    //    return [.. _rawColors[start..(start + HairLength)].Where(x => x != NeutralHair)];
-    //}
+    //TODO fix (ken) Hrothgar are wrong here 
+    public uint[] GetHairColors(Tribes tribe, Genders gender)
+    {
+        var start = GetTribeHairStartIndex(tribe, gender);
+        return [.._rawColors[start..(start + HairLength)].Where(x => x != NeutralHair)];
+    }
 
     public uint[] GetHairHighlightColors() => [.. _rawColors[256..(256 + HairLength)].Where(x => x != NeutralHair)];
 
     public uint[] GetEyeColors() => _rawColors[0..192];
-
-
 
     public uint[] GetFacepaintColors()
     {
@@ -54,19 +53,25 @@ public class HumanData
         return colors;
     }
 
-    public static int GetTribeSkinStartIndex(Tribes tribe, Genders gender)
+    // based on info from https://github.com/imchillin/Anamnesis/blob/af766e20c028ed552c354ecb23d82b6d0218f12d/Anamnesis/Actor/Utilities/ColorData.cs
+
+    private const int COLOR_BYTE_SIZE = 4;
+    private const int UNIQUE_BASE_OFFSET = 0x4800;
+    private const int CHUNK_BYTE_SIZE = 0x1400;
+
+    private const int UNIQUE_BASE_INDEX = UNIQUE_BASE_OFFSET / COLOR_BYTE_SIZE;
+    private const int CHUNK_COLORS_SIZE = CHUNK_BYTE_SIZE / COLOR_BYTE_SIZE;
+
+    private static int GetTribeBaseIndex(Tribes tribe, Genders gender)
     {
-        bool isMasculine = gender == Genders.Masculine;
-        int genderValue = isMasculine ? 0 : 1;
-        int listIndex = ((((int)tribe * 2) + genderValue) * 5) + 3;
-        return listIndex * 256;
+        int genderValue = gender == Genders.Masculine ? 0 : 1;
+        var t = Math.Max(0, (((int)tribe - 1) * 2) + genderValue);
+        return UNIQUE_BASE_INDEX + (t * CHUNK_COLORS_SIZE);
     }
 
+    public static int GetTribeSkinStartIndex(Tribes tribe, Genders gender)
+        => GetTribeBaseIndex(tribe, gender) + 768;      // 768 = 3 * 256
+
     public static int GetTribeHairStartIndex(Tribes tribe, Genders gender)
-    {
-        bool isMasculine = gender == Genders.Masculine;
-        int genderValue = isMasculine ? 0 : 1;
-        int listIndex = ((((int)tribe * 2) + genderValue) * 5) + 4;
-        return listIndex * 256;
-    }
+        => GetTribeBaseIndex(tribe, gender) + 1024;     // 1024 = 4 * 256
 }
